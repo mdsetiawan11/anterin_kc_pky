74767404aa395ba89126ca3cc4cc335a0325e9a5
Muhammad Dadang Setiawan <fzpromds@gmail.com>
2023-10-28 19:36:58 +0700

    permintaan driver

---
+++ lib/pages/driver/permintaan.dart
@@ -0,0 +1,297 @@
+import 'dart:convert';
+
+import 'package:anterin_kc_pky/models/admin/permintaan_model.dart';
+import 'package:anterin_kc_pky/shared/colors.dart';
+import 'package:anterin_kc_pky/shared/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class PermintaanDriver extends StatefulWidget {
+  const PermintaanDriver({super.key});
+
+  @override
+  State<PermintaanDriver> createState() => _PermintaanDriverState();
+}
+
+class _PermintaanDriverState extends State<PermintaanDriver> {
+  String username = '';
+  double initialRating = 0.0;
+  bool isRatingVisibility = false;
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  getData() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    setState(() {
+      username = (localStorage.getString('username') ?? '');
+    });
+  }
+
+  List<PermintaanModel> permintaans = [];
+
+  Future getPermintaan() async {
+    final response =
+        await http.get(Uri.parse('${apiUrl}permintaanbydriver/$username'));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      for (var singleData in data) {
+        PermintaanModel permintaan = PermintaanModel(
+          id: singleData['id'],
+          tanggalPengajuan: DateTime.parse(singleData['tanggal_pengajuan']),
+          usernamePengaju: singleData['username_pengaju'],
+          namaPengaju: singleData['nama_pengaju'],
+          bagianPengaju: singleData['bagian_pengaju'],
+          hari: singleData['hari'],
+          tanggalBerangkat: DateTime.parse(singleData['tanggal_berangkat']),
+          jamBerangkat: singleData['jam_berangkat'],
+          jamKembali: singleData['jam_kembali'],
+          tujuan: singleData['tujuan'],
+          kegiatan: singleData['kegiatan'],
+          usernameDriver: singleData['username_driver'],
+          keterangan: singleData['keterangan'],
+          skor: singleData['skor'],
+          komentar: singleData['komentar'],
+        );
+        permintaans.add(permintaan);
+      }
+    }
+  }
+
+  Future refresh() async {
+    setState(() {
+      permintaans = [];
+    });
+  }
+
+  callback() {
+    setState(() {
+      permintaans = [];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Warna.utama,
+        title: const Text('Permintaan Driver'),
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
+      ),
+      body: RefreshIndicator(
+        onRefresh: refresh,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+              future: getPermintaan(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (permintaans.isEmpty) {
+                  return const Center(
+                    child: Text('Tidak ada data'),
+                  );
+                } else {
+                  return ListView.builder(
+                      itemCount: permintaans.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    (permintaans[index].keterangan ==
+                                            "permintaan")
+                                        ? Card(
+                                            color: Colors.orange,
+                                            elevation: 0,
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Text(
+                                                permintaans[index].keterangan,
+                                                style: const TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                          )
+                                        : (permintaans[index].keterangan ==
+                                                "batal")
+                                            ? Card(
+                                                color: Colors.red,
+                                                elevation: 0,
+                                                child: Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: Text(
+                                                    permintaans[index]
+                                                        .keterangan,
+                                                    style: const TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ),
+                                              )
+                                            : (permintaans[index].keterangan ==
+                                                    "sudah kembali")
+                                                ? Card(
+                                                    color: Warna.utama,
+                                                    elevation: 0,
+                                                    child: Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              8.0),
+                                                      child: Text(
+                                                        permintaans[index]
+                                                            .keterangan,
+                                                        style: const TextStyle(
+                                                            color:
+                                                                Colors.white),
+                                                      ),
+                                                    ),
+                                                  )
+                                                : Card(
+                                                    elevation: 0,
+                                                    color: Colors.green,
+                                                    child: Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              8.0),
+                                                      child: Text(
+                                                        permintaans[index]
+                                                            .keterangan,
+                                                        style: const TextStyle(
+                                                            color:
+                                                                Colors.white),
+                                                      ),
+                                                    ),
+                                                  ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text('Tanggal Pengajuan'),
+                                        Text('Nama Pengaju'),
+                                        Text('Bagian Pengaju'),
+                                        Text('Hari'),
+                                        Text('Tanggal Berangkat'),
+                                        Text('Jam Berangkat'),
+                                        Text('Jam Kembali'),
+                                        Text('Tujuan'),
+                                        Text('Kegiatan'),
+                                        Text('Nama Driver'),
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                            ' : ${permintaans[index].tanggalPengajuan.toString().substring(0, 10)}'),
+                                        Text(
+                                            ' : ${permintaans[index].namaPengaju}'),
+                                        Text(
+                                            ' : ${permintaans[index].bagianPengaju}'),
+                                        Text(' : ${permintaans[index].hari}'),
+                                        Text(
+                                            ' : ${permintaans[index].tanggalBerangkat.toString().substring(0, 10)}'),
+                                        Text(
+                                            ' : ${permintaans[index].jamBerangkat}'),
+                                        Text(
+                                            ' : ${permintaans[index].jamKembali}'),
+                                        Text(' : ${permintaans[index].tujuan}'),
+                                        Text(
+                                            ' : ${permintaans[index].kegiatan}'),
+                                        Text(
+                                            ' : ${permintaans[index].usernameDriver}'),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Visibility(
+                                  visible: (permintaans[index].keterangan ==
+                                          "sudah kembali")
+                                      ? true
+                                      : false,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        const Row(
+                                          children: [
+                                            Text(
+                                              'Penilaian',
+                                              style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        TextFormField(
+                                          initialValue:
+                                              permintaans[index].komentar,
+                                          readOnly: true,
+                                          decoration: InputDecoration(
+                                              border: OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(20),
+                                                  borderSide: const BorderSide(
+                                                      color: Warna.utama))),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        RatingBar.builder(
+                                          initialRating: double.parse(
+                                              permintaans[index].skor),
+                                          direction: Axis.horizontal,
+                                          allowHalfRating: false,
+                                          ignoreGestures: true,
+                                          itemCount: 5,
+                                          itemPadding:
+                                              const EdgeInsets.symmetric(
+                                                  horizontal: 4.0),
+                                          itemBuilder: (context, _) =>
+                                              const Icon(
+                                            Icons.star,
+                                            color: Colors.amber,
+                                          ),
+                                          onRatingUpdate: (rating) {},
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                }
+              }),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/driver/profil.dart
@@ -0,0 +1,252 @@
+import 'package:anterin_kc_pky/pages/auth/intro.dart';
+import 'package:anterin_kc_pky/shared/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lottie/lottie.dart';
+import 'package:material_dialogs/dialogs.dart';
+import 'package:material_dialogs/widgets/buttons/icon_button.dart';
+import 'package:material_dialogs/widgets/buttons/icon_outline_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DriverProfil extends StatefulWidget {
+  const DriverProfil({super.key});
+
+  @override
+  State<DriverProfil> createState() => _DriverProfilState();
+}
+
+class _DriverProfilState extends State<DriverProfil> {
+  String nama = '';
+  String bagian = '';
+
+  final Uri _url = Uri.parse('https://www.instagram.com/md.setiawan11/');
+
+  logout() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    localStorage.remove('username');
+    localStorage.remove('nama');
+    localStorage.remove('role');
+    localStorage.remove('bagian');
+
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => const IntroductionPage()));
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  getData() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    setState(() {
+      nama = (localStorage.getString('nama') ?? '');
+      bagian = (localStorage.getString('bagian') ?? '');
+    });
+  }
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Profil',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Warna.utama,
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Warna.utama,
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image.asset('assets/man.png'),
+                  ),
+                ),
+                Text(
+                  nama,
+                  style: const TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                Text(
+                  bagian,
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Card(
+              shape: Border.all(color: Warna.kedua),
+              elevation: 0,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      child: ListTile(
+                        leading: const Icon(
+                          Icons.privacy_tip,
+                          color: Warna.kedua,
+                        ),
+                        title: Text(
+                          'Privacy Policy',
+                          style: TextStyle(
+                              color: Colors.grey.shade600,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                        shape: const Border(
+                          bottom: BorderSide(color: Warna.kedua),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Dialogs.materialDialog(
+                          color: Colors.white,
+                          title: 'based on flutter develop by mdsdev',
+                          context: context,
+                          actions: [
+                            IconsButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              text: 'Ok',
+                              color: Colors.grey,
+                              textStyle: const TextStyle(color: Colors.white),
+                              iconColor: Colors.white,
+                            ),
+                            IconsButton(
+                              onPressed: () {
+                                _launchUrl();
+                              },
+                              text: 'Instagram',
+                              iconData: FontAwesomeIcons.instagram,
+                              color: Colors.pink,
+                              textStyle: const TextStyle(color: Colors.white),
+                              iconColor: Colors.white,
+                            ),
+                          ],
+                          lottieBuilder: Lottie.asset(
+                            'assets/dev.json',
+                            fit: BoxFit.contain,
+                          ),
+                        );
+                      },
+                      child: ListTile(
+                        leading: const Icon(
+                          Icons.logo_dev,
+                          color: Warna.kedua,
+                        ),
+                        title: Text(
+                          'Developer Info',
+                          style: TextStyle(
+                              color: Colors.grey.shade600,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                        shape: const Border(
+                          bottom: BorderSide(color: Warna.kedua),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Dialogs.materialDialog(
+                            msg: 'Are you sure want to logout?',
+                            title: "Logout",
+                            color: Colors.white,
+                            context: context,
+                            actions: [
+                              IconsOutlineButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                text: 'Cancel',
+                                iconData: Icons.cancel_outlined,
+                                textStyle: const TextStyle(color: Colors.grey),
+                                iconColor: Colors.grey,
+                              ),
+                              IconsButton(
+                                onPressed: () {
+                                  logout();
+                                },
+                                text: 'Yes',
+                                iconData: Icons.logout,
+                                color: Colors.red,
+                                textStyle: const TextStyle(color: Colors.white),
+                                iconColor: Colors.white,
+                              ),
+                            ]);
+                      },
+                      child: ListTile(
+                        leading: const Icon(
+                          Icons.logout,
+                          color: Warna.kedua,
+                        ),
+                        title: Text(
+                          'Logout',
+                          style: TextStyle(
+                              color: Colors.grey.shade600,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                        shape: const Border(
+                          bottom: BorderSide(color: Warna.kedua),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        'Versi 1.0.0',
+                        style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
